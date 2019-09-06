49746624e6bd56eb478e9a3218e5038a6390bff7
github username <github email address>
2019-09-06 09:27:54 +0100

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,4 +1,5 @@
 require_relative "../config/environment.rb"
+# require 'pry'
 
 class Student
 
@@ -34,13 +35,34 @@ class Student
     else
       sql = <<-SQL
       INSERT INTO students(name, grade)
-      VALUE(?, ?)
+      VALUES (?, ?)
       SQL
 
       DB[:conn].execute(sql, self.name, self.grade)
       @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
     end
   end
+
+  def self.create(name, grade)
+    student = Student.new(name, grade)
+    student.save
+    student
+  end
+
+  def self.find_by_name(name)
+    sql = "SELECT * FROM students WHERE name = ?"
+    result = DB[:conn].execute(sql, name)[0]
+
+    Student.new(result[1], result[2], result[0])
+  end
+
+  def self.new_from_db(name)
+    sql = "SELECT * FROM students WHERE name = ?"
+    result = DB[:conn].execute(sql, name)[0]
+# binding.pry
+    Student.new(result[1], result[2], result[0])
+  end
+
 
   def update
     sql = <<-SQL
@@ -50,7 +72,7 @@ class Student
     WHERE id = ?
     SQL
 
-    DB[:conn].execute(sql, name, grade)
+    DB[:conn].execute(sql, self.name, self.grade)
   end
 
 end
